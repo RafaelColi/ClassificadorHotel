ed737a8d60680c0fca6e3aff9b1f0ea47968e898
TioPatinhas2018 <45401268+TioPatinhas2018@users.noreply.github.com>
2021-03-28 17:39:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
+  char nome[10][20] = 'x', cidade[10][20] = 'x', pais[10][20] = 'x';
+  int avaliacao[10] = 0, vaga[10] = 0, area_lazer[10] = -1, piscina[10] = -1, estacionamento[10] = -1;
+  float media[10] = 0.0, preco[10] = 0.0;
+  
+  for(int i = 0; i < 10; i++){
+    if(nome[i] == 'x'){
+      printf("Digite o nome do hotel: ");
+      scanf()
+    }
+  }
+  
+  printf("Digite o nome")
   
   return 0;
 }
